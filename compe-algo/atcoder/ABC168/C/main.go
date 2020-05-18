b18840da02ba90d54ca25dd0f5e094f9287076a4
Kanai Masumi <masumi.net@gmail.com>
2020-05-18 09:26:36 +0900

    Update C with complex

--- compe-algo/atcoder/ABC168/C/main.go
+++ compe-algo/atcoder/ABC168/C/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"math"
+	"math/cmplx"
 	"os"
 	"strconv"
 )
@@ -30,10 +31,9 @@ func parseProblem(r io.Reader) (int, int, int, int) {
 func resolve(a, b, h, m int) float64 {
 	aR := 2 * math.Pi * (float64(h)/12.0 + float64(m)/(60.0*12))
 	bR := 2 * math.Pi * (float64(m) / 60.0)
-	dR := aR - bR
-	dx := float64(b) - float64(a)*math.Cos(dR)
-	dy := float64(a) * math.Sin(dR)
-	return math.Sqrt(dx*dx + dy*dy)
+	ac := cmplx.Rect(float64(a), aR)
+	bc := cmplx.Rect(float64(b), bR)
+	return cmplx.Abs(ac - bc)
 }
 
 // snip-scan-funcs
@@ -41,8 +41,4 @@ func scanInt(sc *bufio.Scanner) int {
 	sc.Scan()
 	i, _ := strconv.Atoi(sc.Text())
 	return int(i)
-}
-func scanString(sc *bufio.Scanner) string {
-	sc.Scan()
-	return sc.Text()
 }
