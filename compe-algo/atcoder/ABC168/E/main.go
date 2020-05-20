ddd657ab80fb949947e46cf8491debff145fc9a5
Kanai Masumi <masumi.net@gmail.com>
2020-05-20 22:43:16 +0900

    Remove useless code

--- compe-algo/atcoder/ABC168/E/main.go
+++ compe-algo/atcoder/ABC168/E/main.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"io"
-	"math"
 	"os"
 	"strconv"
 )
@@ -146,25 +145,6 @@ func moduloMul(a, b, modulo int) int {
 	return a % modulo * b % modulo
 }
 
-func moduloDiv(a, b, modulo int) int {
-	return a % moduloInv(b, modulo) % modulo
-}
-
-func moduloInv(a, modulo int) int {
-	b := modulo
-	u, v := 1, 0
-	for b > 0 {
-		t := a / b
-		a, b = b, a-t*b
-		u, v = v, u-t*v
-	}
-	u %= modulo
-	if u < 0 {
-		u += modulo
-	}
-	return u
-}
-
 func moduloPow(a, b, modulo int) int {
 	res := 1
 	for b > 0 {
@@ -175,73 +155,6 @@ func moduloPow(a, b, modulo int) int {
 		b >>= 1
 	}
 	return res
-}
-
-const length = 510000
-
-var (
-	_fac  = map[int][]int{}
-	_finv = map[int][]int{}
-	_inv  = map[int][]int{}
-)
-
-func moduloCombiInit(modulo int) {
-	fac := make([]int, length)
-	finv := make([]int, length)
-	inv := make([]int, length)
-
-	defer func() {
-		_fac[modulo] = fac
-		_finv[modulo] = finv
-		_inv[modulo] = inv
-	}()
-
-	fac[0], fac[1] = 1, 1
-	finv[0], finv[1] = 1, 1
-	inv[1] = 1
-	for i := 2; i < length; i++ {
-		fac[i] = fac[i-1] * i % modulo
-		inv[i] = modulo - inv[modulo%i]*(modulo/i)%modulo
-		finv[i] = finv[i-1] * inv[i] % modulo
-	}
-}
-
-func moduloCombi(n, k, modulo int) int {
-	if n < k {
-		return 0
-	}
-	if n < 0 || k < 0 {
-		return 0
-	}
-	return _fac[modulo][n] * (_finv[modulo][k] * _finv[modulo][n-k] % modulo) % modulo
-}
-
-func moduloLog(a, b, modulo int) int {
-	// log_a(b)
-	a %= modulo
-	b %= modulo
-	m := int(math.Sqrt(float64(modulo)))
-
-	// basy step
-	values := map[int]int{}
-	val := 1
-	for i := 0; i < m+2; i++ {
-		if _, ok := values[val]; !ok {
-			values[val] = i
-		}
-		val = moduloMul(val, a, modulo)
-	}
-
-	// giant step
-	compound := moduloInv(moduloPow(a, m, modulo), modulo)
-	val = b
-	for i := 0; i < m+2; i++ {
-		if l, ok := values[val]; ok {
-			return (i*m%modulo + l) % modulo
-		}
-		val = moduloMul(val, compound, modulo)
-	}
-	return -1
 }
 
 var mark = struct{}{}
