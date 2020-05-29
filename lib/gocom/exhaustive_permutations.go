6ad1b69d50baa15b6f6d231fa36de7d191db8c66
Kanai Masumi <masumi.net@gmail.com>
2020-05-29 21:55:36 +0900

    Move snip code into lib/gocom

---
+++ lib/gocom/exhaustive_permutations.go
@@ -0,0 +1,36 @@
+package gocom
+
+func permutations(l int) chan []int {
+	ch := make(chan []int)
+	go func() {
+		dfsPermutations(0, make([]bool, l), []int{}, func(perm []int) bool {
+			p := make([]int, len(perm))
+			copy(p, perm)
+			ch <- p
+			return false
+		})
+		close(ch)
+	}()
+	return ch
+}
+
+func dfsPermutations(pos int, used []bool, perm []int, atLeaf func(perm []int) (halt bool)) (halt bool) {
+	l := len(used)
+	if pos == l {
+		if atLeaf(perm) {
+			return true
+		}
+	}
+
+	for i := 0; i < l; i++ {
+		if used[i] {
+			continue
+		}
+		used[i] = true
+		if dfsPermutations(pos+1, used, append(perm, i), atLeaf) {
+			return true
+		}
+		used[i] = false
+	}
+	return false
+}
---
+++ lib/gocom/modulo.go
@@ -0,0 +1,121 @@
+package gocom
+
+import "math"
+
+func moduloAdd(a, b, modulo int) int {
+	result := a%modulo + b%modulo
+	if result < 0 {
+		result += modulo
+	}
+	return result % modulo
+}
+
+func moduloSub(a, b, modulo int) int {
+	result := a%modulo - b%modulo
+	if result < 0 {
+		result += modulo
+	}
+	return result % modulo
+}
+
+func moduloMul(a, b, modulo int) int {
+	return a % modulo * b % modulo
+}
+
+func moduloDiv(a, b, modulo int) int {
+	return a % moduloInv(b, modulo) % modulo
+}
+
+func moduloInv(a, modulo int) int {
+	b := modulo
+	u, v := 1, 0
+	for b > 0 {
+		t := a / b
+		a, b = b, a-t*b
+		u, v = v, u-t*v
+	}
+	u %= modulo
+	if u < 0 {
+		u += modulo
+	}
+	return u
+}
+
+func moduloPow(a, b, modulo int) int {
+	res := 1
+	for b > 0 {
+		if b&1 == 1 {
+			res = res * a % modulo
+		}
+		a = a * a % modulo
+		b >>= 1
+	}
+	return res
+}
+
+const length = 510000
+
+var (
+	_fac  = map[int][]int{}
+	_finv = map[int][]int{}
+	_inv  = map[int][]int{}
+)
+
+func moduloCombiInit(modulo int) {
+	fac := make([]int, length)
+	finv := make([]int, length)
+	inv := make([]int, length)
+
+	defer func() {
+		_fac[modulo] = fac
+		_finv[modulo] = finv
+		_inv[modulo] = inv
+	}()
+
+	fac[0], fac[1] = 1, 1
+	finv[0], finv[1] = 1, 1
+	inv[1] = 1
+	for i := 2; i < length; i++ {
+		fac[i] = fac[i-1] * i % modulo
+		inv[i] = modulo - inv[modulo%i]*(modulo/i)%modulo
+		finv[i] = finv[i-1] * inv[i] % modulo
+	}
+}
+
+func moduloCombi(n, k, modulo int) int {
+	if n < k {
+		return 0
+	}
+	if n < 0 || k < 0 {
+		return 0
+	}
+	return _fac[modulo][n] * (_finv[modulo][k] * _finv[modulo][n-k] % modulo) % modulo
+}
+
+func moduloLog(a, b, modulo int) int {
+	// log_a(b)
+	a %= modulo
+	b %= modulo
+	m := int(math.Sqrt(float64(modulo)))
+
+	// basy step
+	values := map[int]int{}
+	val := 1
+	for i := 0; i < m+2; i++ {
+		if _, ok := values[val]; !ok {
+			values[val] = i
+		}
+		val = moduloMul(val, a, modulo)
+	}
+
+	// giant step
+	compound := moduloInv(moduloPow(a, m, modulo), modulo)
+	val = b
+	for i := 0; i < m+2; i++ {
+		if l, ok := values[val]; ok {
+			return (i*m%modulo + l) % modulo
+		}
+		val = moduloMul(val, compound, modulo)
+	}
+	return -1
+}
