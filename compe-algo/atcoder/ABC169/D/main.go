3a9d78c12a1f980ceff3bd30d08b7a6eace260b8
Kanai Masumi <masumi.net@gmail.com>
2020-06-01 22:22:16 +0900

    'Add directory "ABC169/D" with addir.sh'

---
+++ compe-algo/atcoder/ABC169/D/main.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"strconv"
+)
+
+func main() {
+	fmt.Println(resolve(parseProblem(os.Stdin)))
+}
+
+func parseProblem(r io.Reader) int {
+	const (
+		initialBufSize = 100000
+		maxBufSize     = 1000000
+	)
+	buf := make([]byte, initialBufSize)
+
+	sc := bufio.NewScanner(r)
+	sc.Buffer(buf, maxBufSize)
+	sc.Split(bufio.ScanWords) // bufio.ScanLines
+	n := scanInt(sc)
+	return n
+}
+
+func resolve(n int) int {
+	if n < 2 {
+		return 0
+	}
+	num := n
+	terms := map[int]int{}
+	former := int(math.Sqrt(float64(n))) + 1
+	for p := range primes(former) {
+		pNum := numP(n, p)
+		if pNum != 0 {
+			terms[p] = pNum
+			num /= pow(p, pNum)
+		}
+	}
+	if num != 1 {
+		terms[num] = 1
+	}
+	result := 0
+	for k, iN := range terms {
+		if iN == 0 {
+			continue
+		}
+		for j := 1; j <= iN; j++ {
+			d := pow(k, j)
+			if n%d != 0 {
+				break
+			}
+			n /= d
+			result++
+		}
+	}
+	return result
+}
+
+func pow(n, i int) (res int) {
+	res = 1
+	for i > 0 {
+		if i&1 == 1 {
+			res *= n
+		}
+		i >>= 1
+		n *= n
+	}
+	return res
+}
+
+func isPrime(n int) bool {
+	max := int(math.Sqrt(float64(n)))
+	for i := 2; i <= max+1; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func numP(n, p int) (i int) {
+	for n > 0 {
+		if n%p != 0 {
+			break
+		}
+		i++
+		n = n / p
+	}
+	return i
+}
+
+func eratosthenes(n int) []bool {
+	isPrime := make([]bool, n)
+	for i := 2; i < len(isPrime); i++ {
+		isPrime[i] = true
+	}
+	for i := 2; i < len(isPrime); i++ {
+		if !isPrime[i] {
+			continue
+		}
+		for j := 2; j*i < len(isPrime); j++ {
+			isPrime[j*i] = false
+		}
+	}
+	return isPrime
+}
+
+func primes(n int) chan int {
+	ch := make(chan int)
+	go func() {
+		isPrime := eratosthenes(n)
+		for i := 2; i < len(isPrime); i++ {
+			if !isPrime[i] {
+				continue
+			}
+			ch <- i
+		}
+		close(ch)
+	}()
+	return ch
+}
+
+// snip-scan-funcs
+func scanInt(sc *bufio.Scanner) int {
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	return int(i)
+}
