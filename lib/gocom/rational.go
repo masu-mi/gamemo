c65cad85db14feafb94592c679f5a03a8b640392
Kanai Masumi <masumi.net@gmail.com>
2020-06-01 20:51:11 +0900

    Fix rational

--- lib/gocom/rational.go
+++ lib/gocom/rational.go
@@ -2,6 +2,7 @@ package gocom
 
 import (
 	"bufio"
+	"fmt"
 	"math"
 	"strconv"
 	"strings"
@@ -9,6 +10,13 @@ import (
 
 type rational struct {
 	frac, denomi int
+	neg          int
+}
+
+func newRational(frac, denomi int) rational {
+	r := rational{frac: frac, denomi: denomi}
+	r.canonicalize()
+	return r
 }
 
 func ratMul(x, y rational) rational {
@@ -16,9 +24,7 @@ func ratMul(x, y rational) rational {
 		frac:   x.frac * y.frac,
 		denomi: x.denomi * y.denomi,
 	}
-	d := gcd(r.frac, r.denomi)
-	r.frac /= d
-	r.denomi /= d
+	r.canonicalize()
 	return r
 }
 
@@ -35,9 +41,7 @@ func ratAdd(x, y rational) rational {
 		frac:   x.frac*y.denomi + y.frac*x.denomi,
 		denomi: x.denomi * y.denomi,
 	}
-	d := gcd(r.frac, r.denomi)
-	r.frac /= d
-	r.denomi /= d
+	r.canonicalize()
 	return r
 }
 
@@ -46,7 +50,42 @@ func ratSub(x, y rational) rational {
 }
 
 func (r rational) Float64() float64 {
-	return float64(r.frac) / float64(r.denomi)
+	b := float64(r.frac) / float64(r.denomi)
+	if r.neg == 1 {
+		return -b
+	}
+	return b
+}
+
+func (r rational) String() string {
+	b := fmt.Sprintf("%d/%d", r.frac, r.denomi)
+	if r.neg == 1 {
+		return "-" + b
+	}
+	return b
+}
+
+func (r rational) equal(other rational) bool {
+	r.canonicalize()
+	other.canonicalize()
+
+	return r.neg == other.neg &&
+		r.frac == other.frac &&
+		r.denomi == other.denomi
+}
+
+func (r *rational) canonicalize() {
+	if r.frac < 0 {
+		r.neg ^= 1
+		r.frac = -r.frac
+	}
+	if r.denomi < 0 {
+		r.neg ^= 1
+		r.denomi = -r.denomi
+	}
+	d := gcd(r.frac, r.denomi)
+	r.frac /= d
+	r.denomi /= d
 }
 
 func nextRational(sc *bufio.Scanner) rational {
@@ -55,14 +94,14 @@ func nextRational(sc *bufio.Scanner) rational {
 	if i != -1 {
 		decimalDigit := len(s) - i - 1
 		fr, _ := strconv.Atoi(strings.Replace(s, ".", "", 1))
-		return rational{frac: fr, denomi: int(math.Pow10(decimalDigit))}
+		return newRational(fr, int(math.Pow10(decimalDigit)))
 	}
 	i = strings.Index(s, "/")
 	if i != -1 {
 		fr, _ := strconv.Atoi(s[0:i])
 		de, _ := strconv.Atoi(s[i+1 : len(s)])
-		return rational{frac: fr, denomi: de}
+		return newRational(fr, de)
 	}
 	fr, _ := strconv.Atoi(s)
-	return rational{frac: fr, denomi: 1}
+	return newRational(fr, 1)
 }
--- lib/gocom/rational_test.go
+++ lib/gocom/rational_test.go
@@ -2,8 +2,10 @@ package gocom
 
 import (
 	"bufio"
+	"math/rand"
 	"strings"
 	"testing"
+	"time"
 )
 
 func TestNextRational(t *testing.T) {
@@ -12,18 +14,39 @@ func TestNextRational(t *testing.T) {
 		output rational
 	}
 	for _, tc := range []testCase{
-		testCase{input: "1.45", output: rational{frac: 145, denomi: 100}},
-		testCase{input: ".45", output: rational{frac: 45, denomi: 100}},
-		testCase{input: ".45", output: rational{frac: 45, denomi: 100}},
-		testCase{input: "0.45", output: rational{frac: 45, denomi: 100}},
-		testCase{input: "10.5", output: rational{frac: 105, denomi: 10}},
+		testCase{input: "1.45", output: rational{frac: 29, denomi: 20}},
+		testCase{input: "1.47", output: rational{frac: 147, denomi: 100}},
+		testCase{input: "1.477", output: rational{frac: 1477, denomi: 1000}},
+		testCase{input: ".45", output: rational{frac: 9, denomi: 20}},
+		testCase{input: "0.45", output: rational{frac: 9, denomi: 20}},
+		testCase{input: "10.5", output: rational{frac: 21, denomi: 2}},
 		testCase{input: "145", output: rational{frac: 145, denomi: 1}},
 		testCase{input: "2/3", output: rational{frac: 2, denomi: 3}},
 		testCase{input: "8/35", output: rational{frac: 8, denomi: 35}},
+		testCase{input: "-8/35", output: rational{neg: 1, frac: 8, denomi: 35}},
+		testCase{input: "8/-36", output: rational{neg: 1, frac: 2, denomi: 9}},
+		testCase{input: "-9/-35", output: rational{neg: 0, frac: 9, denomi: 35}},
+		testCase{input: "-10.7", output: rational{neg: 1, frac: 107, denomi: 10}},
 	} {
 		got := nextRational(bufio.NewScanner(strings.NewReader(tc.input)))
 		want := tc.output
 		if got != want {
+			t.Fatalf("want %v, but %v:", want, got)
+		}
+	}
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func TestRatinalOps(t *testing.T) {
+	for i := 0; i < 100; i++ {
+		r := newRational(rand.Int(), rand.Int())
+		ir := ratInv(r)
+		want := newRational(1, 1)
+		got := ratMul(r, ir)
+		if !got.equal(want) {
 			t.Fatalf("want %v, but %v:", want, got)
 		}
 	}
