6ec26a0e563faa9c0fb4d97ffb88405a8c09083c
Kanai Masumi <masumi.net@gmail.com>
2020-06-02 22:21:44 +0900

    'Add directory "ARC017/1" with addir.sh'

---
+++ compe-algo/atcoder/ARC017/1/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+func main() {
+	resolve(parseProblem(os.Stdin))
+}
+
+func parseProblem(r io.Reader) int {
+	const (
+		initialBufSize = 100000
+		maxBufSize     = 1000000
+	)
+	buf := make([]byte, initialBufSize)
+
+	sc := bufio.NewScanner(r)
+	sc.Buffer(buf, maxBufSize)
+	sc.Split(bufio.ScanWords) // bufio.ScanLines
+
+	return scanInt(sc)
+}
+
+func resolve(n int) {
+	for i := 2; i*i <= n; i++ {
+		if n%i == 0 {
+			fmt.Println("NO")
+			return
+		}
+	}
+	fmt.Println("YES")
+	return
+}
+
+// snip-scan-funcs
+func scanInt(sc *bufio.Scanner) int {
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	return int(i)
+}
+func scanString(sc *bufio.Scanner) string {
+	sc.Scan()
+	return sc.Text()
+}
