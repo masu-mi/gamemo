39c84c9c313fd43fcd54ab5b78109454b2b70d26
Kanai Masumi <masumi.net@gmail.com>
2020-06-05 22:53:42 +0900

    'Add directory "ABC084/D" with addir.sh'

--- compe-algo/atcoder/ABC084/D/main.go
+++ compe-algo/atcoder/ABC084/D/main.go
@@ -3,61 +3,70 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"strconv"
 )
 
 func main() {
-	var q int
-	fmt.Scan(&q)
-	type query struct{ l, r int }
-	qs := make([]query, q)
-	sc := bufio.NewScanner(os.Stdin)
-	sc.Split(bufio.ScanWords)
-	var max int
-	for i := 0; i < q; i++ {
-		l := nextInt(sc)
-		r := nextInt(sc)
-		qs[i] = query{l, r}
-		if max < r {
-			max = r
-		}
-	}
-	checkPrime := isPrime(max)
-	culumtive := make([]int, (max+3)/2+1)
+	resolve(parseProblem(os.Stdin))
+}
+
+func parseProblem(r io.Reader) (int, *bufio.Scanner) {
+	const (
+		initialBufSize = 100000
+		maxBufSize     = 1000000
+	)
+	buf := make([]byte, initialBufSize)
+
+	sc := bufio.NewScanner(r)
+	sc.Buffer(buf, maxBufSize)
+	sc.Split(bufio.ScanWords) // bufio.ScanLines
+
+	return scanInt(sc), sc
+}
+
+func resolve(q int, sc *bufio.Scanner) {
+	n := int(1e5)
+	primes := eratosthenes(n + 2)
+	acc := make([]int, n+2)
 	sum := 0
-	for i := 0; i <= (max+3)/2; i++ {
-		culumtive[i] = sum
-		t := i*2 + 1
-		if _, ok := checkPrime[t]; ok {
-			if _, ok := checkPrime[(t+1)/2]; ok {
-				sum++
-			}
+	for i := 1; i <= n+1; i++ {
+		acc[i] = sum
+		if primes[i] && primes[(i+1)/2] {
+			sum++
 		}
 	}
-	for _, q := range qs {
-		fmt.Printf(
-			"%d\n",
-			culumtive[(q.r+3)/2-1]-culumtive[(q.l+1)/2-1],
-		)
+	res := []int{}
+	for i := 0; i < q; i++ {
+		l, r := scanInt(sc), scanInt(sc)
+		res = append(res, acc[r+1]-acc[l])
 	}
+	for _, r := range res {
+		fmt.Println(r)
+	}
+	return
 }
 
-func isPrime(max int) map[int]struct{} {
-	m := map[int]struct{}{}
-	for i := 2; i <= max; i++ {
-		m[i] = struct{}{}
+func eratosthenes(n int) []bool {
+	t := make([]bool, n)
+	for i := 2; i < len(t); i++ {
+		t[i] = true
 	}
-	for i := 2; i <= max; i++ {
-		for j := 2; j <= max/i; j++ {
-			delete(m, i*j)
+	for i := 2; i < len(t); i++ {
+		if !t[i] {
+			continue
+		}
+		for j := 2; j*i < len(t); j++ {
+			t[j*i] = false
 		}
 	}
-	return m
+	return t
 }
 
-func nextInt(sc *bufio.Scanner) int {
+// snip-scan-funcs
+func scanInt(sc *bufio.Scanner) int {
 	sc.Scan()
-	a, _ := strconv.Atoi(sc.Text())
-	return a
+	i, _ := strconv.Atoi(sc.Text())
+	return int(i)
 }
