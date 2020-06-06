b39c2ec9ec7148b7e506cfd832852a2967706c54
Kanai Masumi <masumi.net@gmail.com>
2020-06-06 22:17:58 +0900

    Update ABC122/C

--- compe-algo/atcoder/ABC122/C/main.go
+++ compe-algo/atcoder/ABC122/C/main.go
@@ -1,26 +1,59 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"os"
+	"strconv"
 )
 
 func main() {
-	var n, q int
-	fmt.Scan(&n, &q)
-	var s string
-	fmt.Scan(&s)
-	culumtive := make([]int, n)
-	num := 0
-	for i := 1; i < n; i++ {
-		if s[i] == 'C' && s[i-1] == 'A' {
-			num++
+	parseProblem(os.Stdin)
+}
+
+func parseProblem(r io.Reader) {
+	const (
+		initialBufSize = 100000
+		maxBufSize     = 1000000
+	)
+	buf := make([]byte, initialBufSize)
+
+	sc := bufio.NewScanner(r)
+	sc.Buffer(buf, maxBufSize)
+	sc.Split(bufio.ScanWords) // bufio.ScanLines
+	n, q := scanInt(sc), scanInt(sc)
+	s := scanString(sc)
+	culum := make([]int, n+2)
+	sum := 0
+	for i := 3; i <= n+1; i++ {
+		if s[i-2] == 'C' && s[i-3] == 'A' {
+			sum++
 		}
-		culumtive[i] = num
+		culum[i] = sum
 	}
-	var l, r int
+	res := []int{}
 	for i := 0; i < q; i++ {
-		fmt.Scan(&l, &r)
-		a := culumtive[r-1] - culumtive[l-1]
-		fmt.Printf("%d\n", a)
+		l, r := scanInt(sc), scanInt(sc)
+		res = append(res, culum[r+1]-culum[l+1])
 	}
+	for _, r := range res {
+		fmt.Println(r)
+	}
+	return
+}
+
+func resolve(n int) int {
+	return n
+}
+
+// snip-scan-funcs
+func scanInt(sc *bufio.Scanner) int {
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	return int(i)
+}
+func scanString(sc *bufio.Scanner) string {
+	sc.Scan()
+	return sc.Text()
 }
