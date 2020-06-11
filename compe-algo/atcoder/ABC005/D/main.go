15f55ccb68d6bac6b808fc170ab9c302f9cf8406
Kanai Masumi <masumi.net@gmail.com>
2020-06-11 14:06:59 +0900

    'Add directory "ABC005/D" with addir.sh'

--- compe-algo/atcoder/ABC005/D/main.go
+++ compe-algo/atcoder/ABC005/D/main.go
@@ -1,45 +1,120 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"os"
+	"strconv"
 )
 
+const (
+	initialBufSize = 100000
+	maxBufSize     = 1000000
+)
+
+var sc *bufio.Scanner
+
+func initScanner(r io.Reader) *bufio.Scanner {
+	buf := make([]byte, initialBufSize)
+
+	sc := bufio.NewScanner(r)
+	sc.Buffer(buf, maxBufSize)
+	sc.Split(bufio.ScanWords) // bufio.ScanLines
+	return sc
+}
+
 func main() {
-	var n int
-	fmt.Scan(&n)
-	sums := make([][]int, n+1)
-	sums[0] = make([]int, n+1)
-	for i := 0; i < n; i++ {
-		sums[i+1] = make([]int, n+1)
-		for j := 0; j < n; j++ {
-			var a int
-			fmt.Scan(&a)
-			sums[i+1][j+1] = sums[i+1][j] + sums[i][j+1] - sums[i][j] + a
+	sc = initScanner(os.Stdin)
+	resolve(parseProblem())
+}
+
+func parseProblem() (n, q int, d [][]int) {
+	n = scanInt(sc)
+	d = make([][]int, n)
+	for i := range loop0(n) {
+		d[i] = make([]int, n)
+		for j := range loop0(n) {
+			d[i][j] = scanInt(sc)
 		}
 	}
-	maxScore := make([]int, n*n+1)
-	for li := 0; li < n; li++ {
-		for ri := li + 1; ri < n+1; ri++ {
-			for lj := 0; lj < n; lj++ {
-				for rj := lj + 1; rj < n+1; rj++ {
-					size := (ri - li) * (rj - lj)
-					score := sums[ri][rj] - sums[ri][lj] - sums[li][rj] + sums[li][lj]
-					if maxScore[size] < score {
-						maxScore[size] = score
-					}
+	q = scanInt(sc)
+	return
+}
+
+func loop0(n int) chan int {
+	return loop(0, n-1, 1)
+}
+
+func loop1(n int) chan int {
+	return loop(1, n, 1)
+}
+
+func loop(s, e, d int) chan int {
+	ch := make(chan int)
+	go func() {
+		for i := s; i <= e; i += d {
+			ch <- i
+		}
+		close(ch)
+	}()
+	return ch
+}
+
+func resolve(n, q int, d [][]int) {
+	// init
+	comu := make([][]int, n+1)
+	comu[0] = make([]int, n+1)
+	for i := range loop1(n) {
+		comu[i] = make([]int, n+1)
+		for j := range loop1(n) {
+			comu[i][j] = d[i-1][j-1] +
+				comu[i][j-1] + comu[i-1][j] - comu[i-1][j-1]
+		}
+	}
+	// summarize for size
+	sum := map[int]int{}
+	for xl := 1; xl < n+1; xl++ {
+		for xh := xl; xh < n+1; xh++ {
+			for yl := 1; yl < n+1; yl++ {
+				for yh := yl; yh < n+1; yh++ {
+					// h == l -> 1
+					size := (xh + 1 - xl) * (yh + 1 - yl)
+					// 1..n -> lower part should get -1
+					v := comu[xh][yh] -
+						comu[xh][yl-1] -
+						comu[xl-1][yh] +
+						comu[xl-1][yl-1]
+					sum[size] = max(sum[size], v)
 				}
 			}
 		}
 	}
-	for i := 1; i <= n*n; i++ {
-		if p := maxScore[i-1]; maxScore[i] < p {
-			maxScore[i] = p
+	for range loop0(q) {
+		s := scanInt(sc)
+		m := 0
+		for i := 1; i <= s; i++ {
+			m = max(m, sum[i])
 		}
+		fmt.Println(m)
 	}
-	var q, p int
-	fmt.Scan(&q)
-	for i := 0; i < q; i++ {
-		fmt.Scan(&p)
-		fmt.Printf("%d\n", maxScore[p])
+	return
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
 	}
+	return b
+}
+
+// snip-scan-funcs
+func scanInt(sc *bufio.Scanner) int {
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	return int(i)
+}
+func scanString(sc *bufio.Scanner) string {
+	sc.Scan()
+	return sc.Text()
 }
