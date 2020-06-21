855af112afb6a3dbb658dc5a498c7f1f3897af10
Kanai Masumi <masumi.net@gmail.com>
2020-06-21 22:18:10 +0900

    'Add directory "ABC127/C" with addir.sh'

---
+++ compe-algo/atcoder/ABC127/C/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 1000000
+)
+
+var sc *bufio.Scanner
+
+func initScanner(r io.Reader) *bufio.Scanner {
+	buf := make([]byte, initialBufSize)
+
+	sc := bufio.NewScanner(r)
+	sc.Buffer(buf, maxBufSize)
+	sc.Split(bufio.ScanWords) // bufio.ScanLines
+	return sc
+}
+
+func main() {
+	sc = initScanner(os.Stdin)
+	fmt.Println(resolve(parseProblem()))
+}
+
+func parseProblem() int {
+	n, m := scanInt(sc), scanInt(sc)
+	lmax, rmin := 1, math.MaxInt32
+	for i := 0; i < m; i++ {
+		l, r := scanInt(sc), scanInt(sc)
+		if lmax < l {
+			lmax = l
+		}
+		if r < rmin {
+			rmin = r
+		}
+	}
+	return max(0, min(n, rmin)-lmax+1)
+}
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func resolve(n int) int {
+	return n
+}
+
+// snip-scan-funcs
+func scanInt(sc *bufio.Scanner) int {
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	return int(i)
+}
+func scanString(sc *bufio.Scanner) string {
+	sc.Scan()
+	return sc.Text()
+}
