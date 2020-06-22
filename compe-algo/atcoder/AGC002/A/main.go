4d7a5e96401f36d31d7be7ca50cb21711af5f200
Kanai Masumi <masumi.net@gmail.com>
2020-06-22 12:07:59 +0900

    'Add directory "AGC002/A" with addir.sh'

---
+++ compe-algo/atcoder/AGC002/A/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 1000000
+)
+
+var sc *bufio.Scanner
+
+func initScanner(r io.Reader) *bufio.Scanner {
+	buf := make([]byte, initialBufSize)
+
+	sc := bufio.NewScanner(r)
+	sc.Buffer(buf, maxBufSize)
+	sc.Split(bufio.ScanWords) // bufio.ScanLines
+	return sc
+}
+
+func main() {
+	sc = initScanner(os.Stdin)
+	parseProblem()
+}
+
+func parseProblem() {
+	a, b := scanInt(sc), scanInt(sc)
+	if 0 < a {
+		fmt.Println("Positive")
+		return
+	}
+	if 0 <= b {
+		fmt.Println("Zero")
+		return
+	}
+	nums := b - a + 1
+	if nums%2 == 0 {
+		fmt.Println("Positive")
+	} else {
+		fmt.Println("Negative")
+	}
+	return
+}
+
+func resolve(n int) int {
+	return n
+}
+
+// snip-scan-funcs
+func scanInt(sc *bufio.Scanner) int {
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	return int(i)
+}
+func scanString(sc *bufio.Scanner) string {
+	sc.Scan()
+	return sc.Text()
+}
