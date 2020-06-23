806137fbd12914c30b8d34bfa49cdc64cc8b07d7
Kanai Masumi <masumi.net@gmail.com>
2020-06-23 12:14:01 +0900

    'Add directory "ABC152/C" with addir.sh'

---
+++ compe-algo/atcoder/ABC152/C/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 1000000
+)
+
+var sc *bufio.Scanner
+
+func initScanner(r io.Reader) *bufio.Scanner {
+	buf := make([]byte, initialBufSize)
+
+	sc := bufio.NewScanner(r)
+	sc.Buffer(buf, maxBufSize)
+	sc.Split(bufio.ScanWords) // bufio.ScanLines
+	return sc
+}
+
+func main() {
+	sc = initScanner(os.Stdin)
+	fmt.Println(resolve(parseProblem()))
+}
+
+func parseProblem() int {
+	n := scanInt(sc)
+	count, min := 0, math.MaxInt32
+	for i := 0; i < n; i++ {
+		v := scanInt(sc)
+		if v < min {
+			count++
+			min = v
+		}
+	}
+	return count
+}
+
+func resolve(n int) int {
+	return n
+}
+
+// snip-scan-funcs
+func scanInt(sc *bufio.Scanner) int {
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	return int(i)
+}
+func scanString(sc *bufio.Scanner) string {
+	sc.Scan()
+	return sc.Text()
+}
