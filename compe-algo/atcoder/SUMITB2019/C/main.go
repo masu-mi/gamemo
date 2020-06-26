24a6ac2940828cef198e0ff8aac6234ade2855bc
Kanai Masumi <masumi.net@gmail.com>
2020-06-26 11:02:52 +0900

    'Add directory "SUMITB2019/C" with addir.sh'

---
+++ compe-algo/atcoder/SUMITB2019/C/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 1000000
+)
+
+var sc *bufio.Scanner
+
+func initScanner(r io.Reader) *bufio.Scanner {
+	buf := make([]byte, initialBufSize)
+
+	sc := bufio.NewScanner(r)
+	sc.Buffer(buf, maxBufSize)
+	sc.Split(bufio.ScanWords) // bufio.ScanLines
+	return sc
+}
+
+func main() {
+	sc = initScanner(os.Stdin)
+	fmt.Println(resolve(parseProblem()))
+}
+
+func parseProblem() int {
+	return scanInt(sc)
+}
+
+func resolve(x int) int {
+	result := make([]int, x+1)
+	result[0] = 1
+	for i := 1; i <= x; i++ {
+		for _, v := range []int{100, 101, 102, 103, 104, 105} {
+			if i-v < 0 {
+				continue
+			}
+			if result[i-v] == 1 {
+				result[i] = 1
+				break
+			}
+		}
+	}
+	return result[x]
+}
+
+// snip-scan-funcs
+func scanInt(sc *bufio.Scanner) int {
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	return int(i)
+}
+func scanString(sc *bufio.Scanner) string {
+	sc.Scan()
+	return sc.Text()
+}
