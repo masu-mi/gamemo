87b6bc400a374b404c1b9a49f6adeb83e619e146
Kanai Masumi <masumi.net@gmail.com>
2020-06-29 09:30:51 +0900

    'Add directory "ABC151/C" with addir.sh'

---
+++ compe-algo/atcoder/ABC151/C/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 1000000
+)
+
+var sc *bufio.Scanner
+
+func initScanner(r io.Reader) *bufio.Scanner {
+	buf := make([]byte, initialBufSize)
+
+	sc := bufio.NewScanner(r)
+	sc.Buffer(buf, maxBufSize)
+	sc.Split(bufio.ScanWords) // bufio.ScanLines
+	return sc
+}
+
+func main() {
+	sc = initScanner(os.Stdin)
+	resolve()
+}
+
+func resolve() {
+	n, m := scanInt(sc), scanInt(sc)
+	ok := 0
+	acs := make([]bool, n)
+	penas := make([]int, n)
+	for _ = range loop0(m) {
+		p := scanInt(sc) - 1
+		ans := scanString(sc)
+		if acs[p] {
+			continue
+		}
+		if ans == "AC" {
+			ok++
+			acs[p] = true
+		} else {
+			penas[p]++
+		}
+	}
+	pena := 0
+	for i := range loop0(n) {
+		if acs[i] {
+			pena += penas[i]
+		}
+	}
+	fmt.Printf("%d %d\n", ok, pena)
+}
+
+func loop0(n int) chan int {
+	return loop(0, n-1, 1)
+}
+
+func loop1(n int) chan int {
+	return loop(1, n, 1)
+}
+
+func loop(s, e, d int) chan int {
+	ch := make(chan int)
+	go func() {
+		for i := s; i <= e; i += d {
+			ch <- i
+		}
+		close(ch)
+	}()
+	return ch
+}
+
+// snip-scan-funcs
+func scanInt(sc *bufio.Scanner) int {
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	return int(i)
+}
+func scanString(sc *bufio.Scanner) string {
+	sc.Scan()
+	return sc.Text()
+}
