bf64fdf9cdd4a0ec646558df3ef1b693fdbc7eda
Kanai Masumi <masumi.net@gmail.com>
2020-06-29 20:59:32 +0900

    'Add directory "ABC136/C" with addir.sh'

---
+++ compe-algo/atcoder/ABC136/C/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 1000000
+)
+
+var sc *bufio.Scanner
+
+func initScanner(r io.Reader) *bufio.Scanner {
+	buf := make([]byte, initialBufSize)
+
+	sc := bufio.NewScanner(r)
+	sc.Buffer(buf, maxBufSize)
+	sc.Split(bufio.ScanWords) // bufio.ScanLines
+	return sc
+}
+
+func main() {
+	sc = initScanner(os.Stdin)
+	parseProblem()
+}
+
+func parseProblem() {
+	n := scanInt(sc)
+	hs := nextIntSlice(sc, n)
+	cur := 0
+	for i := 0; i < len(hs); i++ {
+		if !(cur <= hs[i]) {
+			fmt.Println("No")
+			return
+		}
+		if hs[i]-1 >= cur {
+			cur = hs[i] - 1
+		} else {
+			cur = hs[i]
+		}
+	}
+	fmt.Println("Yes")
+	return
+}
+
+func nextInt(sc *bufio.Scanner) int {
+	sc.Scan()
+	a, _ := strconv.Atoi(sc.Text())
+	return int(a)
+}
+
+func nextString(sc *bufio.Scanner) string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextIntSlice(sc *bufio.Scanner, n int) (a []int) {
+
+	a = make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = nextInt(sc)
+	}
+	return a
+}
+
+func resolve(n int) int {
+	return n
+}
+
+// snip-scan-funcs
+func scanInt(sc *bufio.Scanner) int {
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	return int(i)
+}
+func scanString(sc *bufio.Scanner) string {
+	sc.Scan()
+	return sc.Text()
+}
