e9e792a36176073ff15c27b0cb8722b36ef1294b
Kanai Masumi <masumi.net@gmail.com>
2020-06-30 22:37:10 +0900

    'Add directory "AGC029/A" with addir.sh'

---
+++ compe-algo/atcoder/AGC029/A/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 1000000
+)
+
+var sc *bufio.Scanner
+
+func initScanner(r io.Reader) *bufio.Scanner {
+	buf := make([]byte, initialBufSize)
+
+	sc := bufio.NewScanner(r)
+	sc.Buffer(buf, maxBufSize)
+	sc.Split(bufio.ScanWords) // bufio.ScanLines
+	return sc
+}
+
+func main() {
+	sc = initScanner(os.Stdin)
+	fmt.Println(resolve(parseProblem()))
+}
+
+func parseProblem() string {
+	return scanString(sc)
+}
+
+func resolve(str string) int {
+	blackNum := 0
+	opNum := 0
+	for i := 0; i < len(str); i++ {
+		switch str[i] {
+		case 'B':
+			blackNum++
+		case 'W':
+			opNum += blackNum
+		}
+	}
+	return opNum
+}
+
+// snip-scan-funcs
+func scanInt(sc *bufio.Scanner) int {
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	return int(i)
+}
+func scanString(sc *bufio.Scanner) string {
+	sc.Scan()
+	return sc.Text()
+}
