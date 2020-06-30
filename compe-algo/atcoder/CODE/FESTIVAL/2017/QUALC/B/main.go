213a523f70b2f8d917746632d74c2b791f295206
Kanai Masumi <masumi.net@gmail.com>
2020-06-30 22:00:25 +0900

    'Add directory "QUALC/B" with addir.sh'

---
+++ compe-algo/atcoder/CODE/FESTIVAL/2017/QUALC/B/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 1000000
+)
+
+var sc *bufio.Scanner
+
+func initScanner(r io.Reader) *bufio.Scanner {
+	buf := make([]byte, initialBufSize)
+
+	sc := bufio.NewScanner(r)
+	sc.Buffer(buf, maxBufSize)
+	sc.Split(bufio.ScanWords) // bufio.ScanLines
+	return sc
+}
+
+func main() {
+	sc = initScanner(os.Stdin)
+	fmt.Println(resolve(parseProblem()))
+}
+
+func parseProblem() (int, []int) {
+	n := scanInt(sc)
+	as := nextIntSlice(sc, n)
+	return n, as
+}
+
+func nextInt(sc *bufio.Scanner) int {
+	sc.Scan()
+	a, _ := strconv.Atoi(sc.Text())
+	return int(a)
+}
+
+func nextString(sc *bufio.Scanner) string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextIntSlice(sc *bufio.Scanner, n int) (a []int) {
+
+	a = make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = nextInt(sc)
+	}
+	return a
+}
+
+func resolve(n int, as []int) int {
+	// even is includes even item more than or equal to 1
+	// all case - all odd
+	all := pow(3, n)
+	cases := 1
+	for i := 0; i < len(as); i++ {
+		if as[i]%2 == 0 {
+			cases *= 2
+		}
+	}
+	return all - cases
+}
+
+func pow(n, ex int) int {
+	cur := 1
+	for i := 0; i < ex; i++ {
+		cur *= n
+	}
+	return cur
+}
+
+// snip-scan-funcs
+func scanInt(sc *bufio.Scanner) int {
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	return int(i)
+}
+func scanString(sc *bufio.Scanner) string {
+	sc.Scan()
+	return sc.Text()
+}
