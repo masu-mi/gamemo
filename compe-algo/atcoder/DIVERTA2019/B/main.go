c25b23d9ba7eda09d5fda53346bbae9d3f180f06
Kanai Masumi <masumi.net@gmail.com>
2020-07-07 09:09:13 +0900

    'Add directory "DIVERTA2019/B" with addir.sh'

---
+++ compe-algo/atcoder/DIVERTA2019/B/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 1000000
+)
+
+var sc *bufio.Scanner
+
+func initScanner(r io.Reader) *bufio.Scanner {
+	buf := make([]byte, initialBufSize)
+
+	sc := bufio.NewScanner(r)
+	sc.Buffer(buf, maxBufSize)
+	sc.Split(bufio.ScanWords) // bufio.ScanLines
+	return sc
+}
+
+func main() {
+	sc = initScanner(os.Stdin)
+	fmt.Println(resolve(parseProblem()))
+}
+
+func parseProblem() int {
+	r, g, b, n := scanInt(sc), scanInt(sc), scanInt(sc), scanInt(sc)
+	var num int
+	for i := 0; i <= n/r; i++ {
+		for j := 0; j <= (n-r*i)/g; j++ {
+			rest := n - r*i - g*j
+			if rest%b == 0 {
+				num++
+			}
+		}
+	}
+	return num
+}
+
+func resolve(n int) int {
+	return n
+}
+
+// snip-scan-funcs
+func scanInt(sc *bufio.Scanner) int {
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	return int(i)
+}
+func scanString(sc *bufio.Scanner) string {
+	sc.Scan()
+	return sc.Text()
+}
