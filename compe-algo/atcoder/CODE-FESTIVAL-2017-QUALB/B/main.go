a1b2f157dafd576f75bd42141cb2a2e53b681aa5
Kanai Masumi <masumi.net@gmail.com>
2020-07-21 09:31:02 +0900

    'Add directory "CODE-FESTIVAL-2017-QUALB/B" with addir.sh'

---
+++ compe-algo/atcoder/CODE-FESTIVAL-2017-QUALB/B/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 1000000
+)
+
+var sc *bufio.Scanner
+
+func initScanner(r io.Reader) *bufio.Scanner {
+	buf := make([]byte, initialBufSize)
+
+	sc := bufio.NewScanner(r)
+	sc.Buffer(buf, maxBufSize)
+	sc.Split(bufio.ScanWords) // bufio.ScanLines
+	return sc
+}
+
+func main() {
+	sc = initScanner(os.Stdin)
+	parseProblem()
+}
+
+func parseProblem() {
+	n := scanInt(sc)
+	ds := nextIntSlice(sc, n)
+	problems := map[int]int{}
+	for _, v := range ds {
+		problems[v]++
+	}
+	m := scanInt(sc)
+	ts := nextIntSlice(sc, m)
+	for _, v := range ts {
+		rest := problems[v]
+		if rest == 0 {
+			fmt.Println("NO")
+			return
+		}
+		problems[v] = rest - 1
+	}
+	fmt.Println("YES")
+	return
+}
+
+func nextInt(sc *bufio.Scanner) int {
+	sc.Scan()
+	a, _ := strconv.Atoi(sc.Text())
+	return int(a)
+}
+
+func nextString(sc *bufio.Scanner) string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextIntSlice(sc *bufio.Scanner, n int) (a []int) {
+
+	a = make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = nextInt(sc)
+	}
+	return a
+}
+
+// snip-scan-funcs
+func scanInt(sc *bufio.Scanner) int {
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	return int(i)
+}
+func scanString(sc *bufio.Scanner) string {
+	sc.Scan()
+	return sc.Text()
+}
