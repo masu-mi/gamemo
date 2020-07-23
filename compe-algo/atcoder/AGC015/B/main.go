bf4013f764115f8ff27b40a8bd210d79a71a4740
Kanai Masumi <masumi.net@gmail.com>
2020-07-23 11:29:34 +0900

    'Add directory "AGC015/B" with addir.sh'

---
+++ compe-algo/atcoder/AGC015/B/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 1000000
+)
+
+var sc *bufio.Scanner
+
+func initScanner(r io.Reader) *bufio.Scanner {
+	buf := make([]byte, initialBufSize)
+
+	sc := bufio.NewScanner(r)
+	sc.Buffer(buf, maxBufSize)
+	sc.Split(bufio.ScanWords) // bufio.ScanLines
+	return sc
+}
+
+func main() {
+	sc = initScanner(os.Stdin)
+	fmt.Println(resolve())
+}
+
+func resolve() int {
+	str := scanString(sc)
+	sum := 0
+	for i := range str {
+		downStaiers := i + 1 - 1
+		upStairs := len(str) - 1 - i
+		switch str[i] {
+		case 'U':
+			sum += upStairs + 2*downStaiers
+		case 'D':
+			sum += 2*upStairs + downStaiers
+		}
+	}
+	return sum
+}
+
+// snip-scan-funcs
+func scanInt(sc *bufio.Scanner) int {
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	return int(i)
+}
+func scanString(sc *bufio.Scanner) string {
+	sc.Scan()
+	return sc.Text()
+}
