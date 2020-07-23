8a729d69b137bfe0a9c1b7caf999ec233127e3f2
Kanai Masumi <masumi.net@gmail.com>
2020-07-23 13:22:25 +0900

    'Add directory "ARC069/A" with addir.sh'

---
+++ compe-algo/atcoder/ARC069/A/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 1000000
+)
+
+var sc *bufio.Scanner
+
+func initScanner(r io.Reader) *bufio.Scanner {
+	buf := make([]byte, initialBufSize)
+
+	sc := bufio.NewScanner(r)
+	sc.Buffer(buf, maxBufSize)
+	sc.Split(bufio.ScanWords) // bufio.ScanLines
+	return sc
+}
+
+func main() {
+	sc = initScanner(os.Stdin)
+	fmt.Println(resolve(parseProblem()))
+}
+
+func parseProblem() (int, int) {
+	n, m := scanInt(sc), scanInt(sc)
+	return n, m
+}
+
+func resolve(n, m int) int {
+	numUseS := min(n, m>>1)
+	numLeftC := m - numUseS<<1
+	return numUseS + numLeftC>>2
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// snip-scan-funcs
+func scanInt(sc *bufio.Scanner) int {
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	return int(i)
+}
+func scanString(sc *bufio.Scanner) string {
+	sc.Scan()
+	return sc.Text()
+}
