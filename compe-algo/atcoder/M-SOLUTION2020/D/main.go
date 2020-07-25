70e070f9884b6a1575233e6ed8a28e6031777316
Kanai Masumi <masumi.net@gmail.com>
2020-07-25 21:41:36 +0900

    'Add directory "M-SOLUTION2020/D" with addir.sh'

---
+++ compe-algo/atcoder/M-SOLUTION2020/D/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 1000000
+)
+
+var sc *bufio.Scanner
+
+func initScanner(r io.Reader) *bufio.Scanner {
+	buf := make([]byte, initialBufSize)
+
+	sc := bufio.NewScanner(r)
+	sc.Buffer(buf, maxBufSize)
+	sc.Split(bufio.ScanWords) // bufio.ScanLines
+	return sc
+}
+
+func main() {
+	sc = initScanner(os.Stdin)
+	fmt.Println(resolve(parseProblem()))
+}
+
+func parseProblem() (int, []int) {
+	n := scanInt(sc)
+	as := nextIntSlice(sc, n)
+	return n, as
+}
+
+func nextInt(sc *bufio.Scanner) int {
+	sc.Scan()
+	a, _ := strconv.Atoi(sc.Text())
+	return int(a)
+}
+
+func nextString(sc *bufio.Scanner) string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextIntSlice(sc *bufio.Scanner, n int) (a []int) {
+
+	a = make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = nextInt(sc)
+	}
+	return a
+}
+
+const (
+	increasing = 0 + iota
+	decreasing
+)
+
+func resolve(n int, as []int) int {
+	budget := 1000
+	stocks := 0
+	as = append(as, 0) // make it easy
+	for i := 0; i+1 < len(as); i++ {
+		if as[i] <= as[i+1] {
+			stocks += budget / as[i]
+			budget = budget % as[i]
+		} else {
+			budget += stocks * as[i]
+			stocks = 0
+		}
+	}
+	return budget
+}
+
+// snip-scan-funcs
+func scanInt(sc *bufio.Scanner) int {
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	return int(i)
+}
+func scanString(sc *bufio.Scanner) string {
+	sc.Scan()
+	return sc.Text()
+}
