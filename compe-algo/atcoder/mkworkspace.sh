c61c7ae8fc487f0c001bca904026b4ac0bac6b77
Kanai Masumi <masumi.net@gmail.com>
2020-07-28 23:12:18 +0900

    Change mkworkspace.sh

--- compe-algo/atcoder/mkworkspace.sh
+++ compe-algo/atcoder/mkworkspace.sh
@@ -1,8 +1,10 @@
-#!/Users/masumi/local/bin/bash
+#!/usr/bin/env bash
 
 url=$1
 problem=$(basename $url)
+# dir=$(echo ${problem} | sed -E 's/_/\//g')
 dir=$(echo ${problem^^} | sed -E 's/_/\//g')
+echo $dir
 
 mkdir -p $dir
 touch $dir/main.go
