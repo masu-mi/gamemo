b818153039b16692852c276c216f0b812fd0ebc5
Kanai Masumi <masumi.net@gmail.com>
2020-07-28 23:07:54 +0900

    'Add directory "ABC131/D" with addir.sh'

---
+++ compe-algo/atcoder/ABC131/D/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 1000000
+)
+
+var sc *bufio.Scanner
+
+func initScanner(r io.Reader) *bufio.Scanner {
+	buf := make([]byte, initialBufSize)
+
+	sc := bufio.NewScanner(r)
+	sc.Buffer(buf, maxBufSize)
+	sc.Split(bufio.ScanWords) // bufio.ScanLines
+	return sc
+}
+
+func main() {
+	sc = initScanner(os.Stdin)
+	if resolve(parseProblem()) {
+		fmt.Println("Yes")
+		return
+	}
+	fmt.Println("No")
+}
+
+func parseProblem() (int, []task) {
+	n := scanInt(sc)
+	result := make([]task, n)
+	for i := 0; i < n; i++ {
+		result[i] = task{
+			take:  scanInt(sc),
+			limit: scanInt(sc),
+		}
+	}
+	return n, result
+}
+
+type task struct{ take, limit int }
+type tasks []task
+
+func (t tasks) Len() int {
+	return len(t)
+}
+
+func (t tasks) Less(i, j int) bool {
+	return t[i].limit < t[j].limit
+}
+
+func (t tasks) Swap(i, j int) {
+	t[i], t[j] = t[j], t[i]
+}
+
+func resolve(n int, ts []task) bool {
+	sort.Sort(sort.Reverse(tasks(ts)))
+	limit := math.MaxInt32
+	for _, t := range ts {
+		if t.limit <= limit {
+			limit = t.limit
+		}
+		limit -= t.take
+	}
+	return limit >= 0
+}
+
+// snip-scan-funcs
+func scanInt(sc *bufio.Scanner) int {
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	return int(i)
+}
+func scanString(sc *bufio.Scanner) string {
+	sc.Scan()
+	return sc.Text()
+}
