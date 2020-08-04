318804c96a6bd39552c4f4dbcd9359db522b926d
Kanai Masumi <masumi.net@gmail.com>
2020-08-04 12:30:06 +0900

    Fix Vol1611

--- compe-algo/aoj/Vol/1611/main.go
+++ compe-algo/aoj/Vol/1611/main.go
@@ -9,8 +9,8 @@ import (
 )
 
 const (
-	initialBufSize = 100000
-	maxBufSize     = 1000000
+	initialBufSize = 10000000
+	maxBufSize     = 100000000
 )
 
 var sc *bufio.Scanner
@@ -40,7 +40,6 @@ func nextString(sc *bufio.Scanner) string {
 }
 
 func nextIntSlice(sc *bufio.Scanner, n int) (a []int) {
-
 	a = make([]int, n)
 	for i := 0; i < n; i++ {
 		a[i] = nextInt(sc)
@@ -54,20 +53,23 @@ func initDP(n int) {
 	dp = make([][]int, n+1)
 	for i := 0; i < len(dp); i++ {
 		dp[i] = make([]int, n+1)
+		for j := 0; j < len(dp[0]); j++ {
+			dp[i][j] = -1
+		}
 	}
 }
 
 func resolve() {
 	ans := []int{}
 	for true {
-		n := scanInt(sc)
+		n := nextInt(sc)
 		if n == 0 {
 			break
 		}
 		as := nextIntSlice(sc, n)
 		initDP(n)
 		_rec(as, n, 0, n)
-		ans = append(ans, dp[0][n]*2)
+		ans = append(ans, dp[0][n])
 	}
 	for _, v := range ans {
 		fmt.Println(v)
@@ -76,52 +78,43 @@ func resolve() {
 }
 
 func _rec(as []int, n, l, r int) int {
-	if dp[l][r] != 0 {
+	if dp[l][r] > -1 {
 		return dp[l][r]
 	}
-	if r-l == 1 {
-		// prevent to hit daruma
-		return 0
+	if r-l == 1 { // prevent to hit daruma
+		dp[l][r] = 0
+		return dp[l][r]
 	}
 	if r-l == 2 {
-		var v int
 		if abs(as[l]-as[r-1]) <= 1 {
-			v = 1
+			dp[l][r] = 2
+		} else {
+			dp[l][r] = 0
 		}
-		dp[l][r] = v
-		return v
+		return dp[l][r]
 	}
 	if abs(as[l]-as[r-1]) <= 1 {
 		v := _rec(as, n, l+1, r-1)
 		// if we can remove internal blocks
-		if (r - 1 - (l + 1)) == 2*v {
-			dp[l][r] = v + 1
+		if (r - 1 - (l + 1)) == v {
+			dp[l][r] = v + 2
+			return dp[l][r]
 		} else {
 			dp[l][r] = v
 		}
 	}
-	for i := 1; i < r-l; i++ {
-		v := _rec(as, n, l, l+i) + _rec(as, n, l+i, r)
+	for i := l + 1; i < r; i++ {
+		v := _rec(as, n, l, i) + _rec(as, n, i, r)
 		if dp[l][r] < v {
 			dp[l][r] = v
 		}
 	}
 	return dp[l][r]
 }
+
 func abs(v int) int {
 	if v < 0 {
 		return -v
 	}
 	return v
-}
-
-// snip-scan-funcs
-func scanInt(sc *bufio.Scanner) int {
-	sc.Scan()
-	i, _ := strconv.Atoi(sc.Text())
-	return int(i)
-}
-func scanString(sc *bufio.Scanner) string {
-	sc.Scan()
-	return sc.Text()
 }
