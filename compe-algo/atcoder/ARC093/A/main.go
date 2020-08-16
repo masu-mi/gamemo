9b917542a69ee224e8ee8284bc646414f5739390
Kanai Masumi <masumi.net@gmail.com>
2020-08-17 08:40:45 +0900

    'Add directory "ARC093/A" with addir.sh'

---
+++ compe-algo/atcoder/ARC093/A/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 1000000
+)
+
+var sc *bufio.Scanner
+
+func initScanner(r io.Reader) *bufio.Scanner {
+	buf := make([]byte, initialBufSize)
+
+	sc := bufio.NewScanner(r)
+	sc.Buffer(buf, maxBufSize)
+	sc.Split(bufio.ScanWords) // bufio.ScanLines
+	return sc
+}
+
+func main() {
+	sc = initScanner(os.Stdin)
+	resolve(parseProblem())
+}
+
+func parseProblem() (int, []int) {
+	n := scanInt(sc)
+	as := make([]int, n+2)
+	for i := 1; i <= n; i++ {
+		as[i] = scanInt(sc)
+	}
+	return n, as
+}
+
+// package: gocom
+// packed src of [/Users/masumi/dev/src/github.com/masu-mi/gamemo/lib/gocom/next.go] with goone.
+
+func nextInt(sc *bufio.Scanner) int {
+	sc.Scan()
+	a, _ := strconv.Atoi(sc.Text())
+	return int(a)
+}
+
+func nextString(sc *bufio.Scanner) string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextIntSlice(sc *bufio.Scanner, n int) (a []int) {
+
+	a = make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = nextInt(sc)
+	}
+	return a
+}
+func resolve(n int, as []int) {
+	sum := 0
+	for i := 1; i < len(as); i++ {
+		sum += abs(as[i] - as[i-1])
+	}
+	for i := 1; i <= n; i++ {
+		v := sum - (abs(as[i]-as[i-1]) + abs(as[i+1]-as[i])) + abs(as[i+1]-as[i-1])
+		fmt.Println(v)
+	}
+}
+
+func abs(v int) int {
+	if v < 0 {
+		return -v
+	}
+	return v
+}
+
+// snip-scan-funcs
+func scanInt(sc *bufio.Scanner) int {
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	return int(i)
+}
+func scanString(sc *bufio.Scanner) string {
+	sc.Scan()
+	return sc.Text()
+}
