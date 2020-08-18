92dd5e2438f7b667825ebf81193c949ae47d527c
Kanai Masumi <masumi.net@gmail.com>
2020-08-18 23:10:00 +0900

    'Add directory "AGC011/A" with addir.sh'

--- compe-algo/atcoder/AGC011/A/main.go
+++ compe-algo/atcoder/AGC011/A/main.go
@@ -3,37 +3,96 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"math"
+	"io"
 	"os"
 	"sort"
 	"strconv"
 )
 
+const (
+	initialBufSize = 100000
+	maxBufSize     = 1000000
+)
+
+var sc *bufio.Scanner
+
+func initScanner(r io.Reader) *bufio.Scanner {
+	buf := make([]byte, initialBufSize)
+
+	sc := bufio.NewScanner(r)
+	sc.Buffer(buf, maxBufSize)
+	sc.Split(bufio.ScanWords) // bufio.ScanLines
+	return sc
+}
+
 func main() {
-	var n, c, k int
-	fmt.Scan(&n, &c, &k)
-	sc := bufio.NewScanner(os.Stdin)
-	sc.Split(bufio.ScanWords)
-	ts := make([]int, 0, n)
-	for i := 0; i < n; i++ {
-		sc.Scan()
-		a, _ := strconv.Atoi(sc.Text())
-		ts = append(ts, a)
-	}
+	sc = initScanner(os.Stdin)
+	n, c, k := scanInt(sc), scanInt(sc), scanInt(sc)
+	ts := nextIntSlice(sc, n)
+	fmt.Println(resolve(n, c, k, ts))
+}
+
+func resolve(n, c, k int, ts []int) int {
 	sort.Sort(sort.IntSlice(ts))
-	limit, load, numOfBus := -math.MaxInt32, 0, 0
-	for _, t := range ts {
-		if t > limit {
-			numOfBus++
-			load = 1
-			limit = t + k
-		} else if load == c {
-			numOfBus++
-			load = 1
-			limit = t + k
-		} else {
-			load++
+	var idx, trackNum int
+
+	limit := ts[0] + k
+	idx, sum := 0, 1
+	for i := 1; i < len(ts); i++ {
+		if sum == c {
+			sum = 0
+			trackNum++
+			idx += c
+			limit = ts[idx] + k
+		} else if limit < ts[i] {
+			sum = 0
+			trackNum++
+			idx = i
+			limit = ts[idx] + k
 		}
+		sum++
 	}
-	fmt.Printf("%d\n", numOfBus)
+	trackNum += (sum + c - 1) / c
+	return trackNum
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// package: gocom
+// packed src of [/Users/masumi/dev/src/github.com/masu-mi/gamemo/lib/gocom/next.go] with goone.
+
+func nextInt(sc *bufio.Scanner) int {
+	sc.Scan()
+	a, _ := strconv.Atoi(sc.Text())
+	return int(a)
+}
+
+func nextString(sc *bufio.Scanner) string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextIntSlice(sc *bufio.Scanner, n int) (a []int) {
+
+	a = make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = nextInt(sc)
+	}
+	return a
+}
+
+// snip-scan-funcs
+func scanInt(sc *bufio.Scanner) int {
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	return int(i)
+}
+func scanString(sc *bufio.Scanner) string {
+	sc.Scan()
+	return sc.Text()
 }
