129174fc86a2c90f9e216d750420d36e31575a4d
Kanai Masumi <masumi.net@gmail.com>
2021-01-20 16:53:24 +0900

    'Add directory "ABC057/C" with addir.sh'

--- compe-algo/atcoder/ABC057/C/main.go
+++ compe-algo/atcoder/ABC057/C/main.go
@@ -1,36 +1,65 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"math"
+	"os"
+	"strconv"
 )
 
+const (
+	initialBufSize = 100000
+	maxBufSize     = 1000000
+)
+
+var sc *bufio.Scanner
+
+func initScanner(r io.Reader) *bufio.Scanner {
+	buf := make([]byte, initialBufSize)
+
+	sc := bufio.NewScanner(r)
+	sc.Buffer(buf, maxBufSize)
+	sc.Split(bufio.ScanWords) // bufio.ScanLines
+	return sc
+}
+
 func main() {
-	var n int
-	fmt.Scan(&n)
-	l := int(math.Sqrt(float64(n)))
-	min := math.MaxInt32
-	for i := 1; i <= l; i++ {
+	sc = initScanner(os.Stdin)
+	fmt.Println(resolve())
+}
+
+func resolve() int {
+	n := scanInt(sc)
+	minV := math.MaxInt64
+	for i := 1; i*i <= n; i++ {
 		if n%i == 0 {
-			var cmin int
-			if di, dni := digits(i), digits(n/i); di < dni {
-				cmin = dni
-			} else {
-				cmin = di
-			}
-			if cmin < min {
-				min = cmin
+			div := n / i
+			v := fVal(i, div)
+			if minV > v {
+				minV = v
 			}
 		}
 	}
-	fmt.Printf("%d\n", min)
+	return minV
 }
 
-func digits(n int) (d int) {
-	tmp := n
-	for tmp > 0 {
-		d++
-		tmp /= 10
+func fVal(a, b int) int {
+	aL, bL := len(strconv.Itoa(a)), len(strconv.Itoa(b))
+	if aL < bL {
+		return bL
 	}
-	return d
+	return aL
+}
+
+// snip-scan-funcs
+func scanInt(sc *bufio.Scanner) int {
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	return int(i)
+}
+func scanString(sc *bufio.Scanner) string {
+	sc.Scan()
+	return sc.Text()
 }
