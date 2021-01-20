d17abfe4f52fdf707caa02aa0bd88f53ee1ca516
Kanai Masumi <masumi.net@gmail.com>
2021-01-20 16:43:04 +0900

    'Add directory "ABC106/B" with addir.sh'

--- compe-algo/atcoder/ABC106/B/main.go
+++ compe-algo/atcoder/ABC106/B/main.go
@@ -1,38 +1,70 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"os"
+	"strconv"
 )
 
-func main() {
-	var n int
-	fmt.Scanf("%d", &n)
-	fmt.Printf("%d\n", find(n))
+const (
+	initialBufSize = 100000
+	maxBufSize     = 1000000
+)
+
+var sc *bufio.Scanner
+
+func initScanner(r io.Reader) *bufio.Scanner {
+	buf := make([]byte, initialBufSize)
+
+	sc := bufio.NewScanner(r)
+	sc.Buffer(buf, maxBufSize)
+	sc.Split(bufio.ScanWords) // bufio.ScanLines
+	return sc
 }
 
-func find(n int) int {
+func main() {
+	sc = initScanner(os.Stdin)
+	fmt.Println(resolve())
+}
+
+func resolve() int {
+	n := scanInt(sc)
 	var num int
 	for i := 1; i <= n; i++ {
-		if i%2 == 1 && divisorNumIs(i, 8) {
+		if i%2 == 0 {
+			continue
+		}
+		if strangeNumber(i) {
 			num++
 		}
 	}
 	return num
 }
 
-func divisorNumIs(n, divisor int) bool {
-	life := divisor - 2
-	for i := 2; i*i <= n; i++ {
-		if n%i == 0 {
-			if n/i == i {
-				life--
+func strangeNumber(i int) bool {
+	var num int
+	for j := 1; j*j <= i; j++ {
+		if i%j == 0 {
+			dv := i / j
+			if dv == j {
+				num++
 			} else {
-				life -= 2
+				num += 2
 			}
 		}
-		if life < 0 {
-			break
-		}
 	}
-	return life == 0
+	return num == 8
+}
+
+// snip-scan-funcs
+func scanInt(sc *bufio.Scanner) int {
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	return int(i)
+}
+func scanString(sc *bufio.Scanner) string {
+	sc.Scan()
+	return sc.Text()
 }
