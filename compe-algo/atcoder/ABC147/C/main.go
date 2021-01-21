af3a486cb62852ded6e434817d3874ec301985c2
Kanai Masumi <masumi.net@gmail.com>
2021-01-21 15:42:59 +0900

    'Add directory "ABC147/C" with addir.sh'

--- compe-algo/atcoder/ABC147/C/main.go
+++ compe-algo/atcoder/ABC147/C/main.go
@@ -8,110 +8,84 @@ import (
 	"strconv"
 )
 
-const (
-	initialBufSize = 100000
-	maxBufSize     = 1000000
-)
-
-var sc *bufio.Scanner
-
-func initScanner(r io.Reader) *bufio.Scanner {
-	buf := make([]byte, initialBufSize)
-
-	sc := bufio.NewScanner(r)
-	sc.Buffer(buf, maxBufSize)
-	sc.Split(bufio.ScanWords) // bufio.ScanLines
-	return sc
-}
-
 func main() {
-	sc = initScanner(os.Stdin)
-	fmt.Println(resolve())
+	fmt.Printf("%d\n", resolve(parseProblem(os.Stdin)))
 }
 
-func resolve() int {
-	claims := map[int]map[int]int{}
-	n := scanInt(sc)
+func parseProblem(r io.Reader) int {
+	var n int
+	fmt.Fscanf(r, "%d", &n)
+	sc := bufio.NewScanner(r)
+	sc.Split(bufio.ScanWords)
+	testimonies := []testimony{}
 	for i := 0; i < n; i++ {
-		a := scanInt(sc)
-		m := map[int]int{}
-		for j := 0; j < a; j++ {
-			x, y := scanInt(sc)-1, scanInt(sc)
-			m[x] = y
-		}
-		claims[i] = m
+		testimonies = append(testimonies, parseTestimony(sc))
 	}
-	maxNum := 0
-	for i := 0; i < 1<<uint(n); i++ {
-		num := onesCount(uint64(i))
-		if maxNum > num {
-			continue
-		}
-		if acceptable(claims, n, i) {
-			maxNum = num
-		}
-	}
-	return maxNum
-}
-
-func acceptable(claims map[int]map[int]int, num, state int) bool {
-	for i := 0; i < num; i++ {
-		if state>>uint(i)&1 != 0 {
-			for k, v := range claims[i] {
-				if state>>uint(k)&1 != v {
-					return false
-				}
+	max := uint32(0)
+	for i := 0; i < 1<<uint32(n); i++ {
+		if checkAllHornest(n, uint32(i), testimonies) {
+			numH := onesCount(uint32(i))
+			if max < numH {
+				max = numH
 			}
 		}
+	}
+	return int(max)
+}
+
+func checkAllHornest(n int, i uint32, testimonies []testimony) bool {
+	flag := 1
+	idx := 0
+	for idx < n {
+		if i&uint32(flag) == uint32(flag) {
+			if !testimonies[idx].accept(i) {
+				return false
+			}
+		}
+		flag <<= 1
+		idx++
 	}
 	return true
 }
 
-// package: gocom
-// packed src of [/Users/masumi/dev/src/github.com/masu-mi/gamemo/lib/gocom/bits_util.go] with goone.
-
-func onesCount(x uint64) (num int) {
-	const m0 = 0x5555555555555555
-	const m1 = 0x3333333333333333
-	const m2 = 0x0f0f0f0f0f0f0f0f
-
-	const m = 1<<64 - 1
-	x = x>>1&(m0&m) + x&(m0&m)
-	x = x>>2&(m1&m) + x&(m1&m)
-	x = (x>>4 + x) & (m2 & m)
-	x += x >> 8
-	x += x >> 16
-	x += x >> 32
-	return int(x) & (1<<7 - 1)
+func onesCount(bits uint32) (num uint32) {
+	num = (bits >> 1) & 03333333333
+	num = bits - num - ((num >> 1) & 03333333333)
+	num = ((num + (num >> 3)) & 0707070707) % 077
+	return
 }
 
-func ntz(bits uint64) (num int) {
+type testimony struct{ h, l uint32 }
 
-	return onesCount(bits&(-bits) - 1)
+func (tt testimony) accept(state uint32) bool {
+	if state&tt.l != 0 {
+		return false
+	}
+	if state|tt.h != state {
+		return false
+	}
+	return true
 }
 
-func nlz(bits uint64) (num int) {
-
-	bits = bits | (bits >> 1)
-	bits = bits | (bits >> 2)
-	bits = bits | (bits >> 4)
-	bits = bits | (bits >> 8)
-	bits = bits | (bits >> 16)
-	bits = bits | (bits >> 32)
-	return onesCount(^bits)
-}
-
-func refBit(i uint64, b uint) int {
-	return int((i >> b) & 1)
-}
-
-// snip-scan-funcs
-func scanInt(sc *bufio.Scanner) int {
+func parseTestimony(sc *bufio.Scanner) (t testimony) {
 	sc.Scan()
-	i, _ := strconv.Atoi(sc.Text())
-	return int(i)
+	n, _ := strconv.Atoi(sc.Text())
+
+	t.l, t.h = 0, 0
+	for i := 0; i < n; i++ {
+		sc.Scan()
+		idx, _ := strconv.Atoi(sc.Text())
+		sc.Scan()
+		horneset, _ := strconv.Atoi(sc.Text())
+		if horneset == 0 {
+			t.l |= 1 << uint32(idx-1)
+		} else {
+			t.h |= 1 << uint32(idx-1)
+		}
+	}
+	return
 }
-func scanString(sc *bufio.Scanner) string {
-	sc.Scan()
-	return sc.Text()
+
+func resolve(n int) int {
+	return n
 }
