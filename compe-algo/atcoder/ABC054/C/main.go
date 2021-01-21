d81701889d71d7ed443e3f4e17d5cad217cdeedc
Kanai Masumi <masumi.net@gmail.com>
2021-01-21 16:54:08 +0900

    'Add directory "ABC054/C" with addir.sh'

--- compe-algo/atcoder/ABC054/C/main.go
+++ compe-algo/atcoder/ABC054/C/main.go
@@ -31,148 +31,99 @@ func main() {
 
 func resolve() int {
 	n, m := scanInt(sc), scanInt(sc)
-	ll := nextLinkedList(n, m, 1, sc)
-	dfs(ll, 0, n)
-	return num
-}
-
-var num int
-var visited map[int]bool = map[int]bool{}
-
-func dfs(ll *basicLinkedList, start, n int) bool {
-	visited[start] = true
-	defer func() {
-		delete(visited, start)
-	}()
-	if len(visited) == n {
-		num++
-		return false
+	ps := newSet()
+	for i := 0; i < m; i++ {
+		x, y := scanInt(sc)-1, scanInt(sc)-1
+		ps.add(pair{x, y})
+		ps.add(pair{y, x})
 	}
-	for nextNode := range ll.edges[start].members() {
-		if _, ok := visited[nextNode]; ok {
-			continue
+
+	count := 0
+	for p := range permutations(n - 1) {
+		if allExists(ps, p) {
+			count++
 		}
-		dfs(ll, nextNode, n)
 	}
-	return false
+	return count
 }
 
-// package: gocom
-// packed src of [/Users/masumi/dev/src/github.com/masu-mi/gamemo/lib/gocom/none.go /Users/masumi/dev/src/github.com/masu-mi/gamemo/lib/gocom/next.go /Users/masumi/dev/src/github.com/masu-mi/gamemo/lib/gocom/linkedlist.go /Users/masumi/dev/src/github.com/masu-mi/gamemo/lib/gocom/set.go] with goone.
-
-type none struct{}
-
-var mark none
-
-func nextInt(sc *bufio.Scanner) int {
-	sc.Scan()
-	a, _ := strconv.Atoi(sc.Text())
-	return int(a)
-}
-
-func nextString(sc *bufio.Scanner) string {
-	sc.Scan()
-	return sc.Text()
-}
-
-func nextIntSlice(sc *bufio.Scanner, n int) (a []int) {
-
-	a = make([]int, n)
-	for i := 0; i < n; i++ {
-		a[i] = nextInt(sc)
+func allExists(ps pairSet, p []int) bool {
+	cur := -1
+	for i := 0; i < len(p); i++ {
+		next := p[i]
+		if !ps.doesContain(pair{cur + 1, next + 1}) {
+			return false
+		}
+		cur = next
 	}
-	return a
+	return true
 }
 
-type basicLinkedList struct {
-	size, deg int
-	edges     []intSet
+type pair struct{ x, y int }
+
+type pairSet map[pair]none
+
+func newSet() pairSet {
+	return make(map[pair]none)
 }
 
-func newLinkedList(size int) *basicLinkedList {
-	ll := &basicLinkedList{size: size, edges: make([]intSet, size)}
-	for i := 0; i < size; i++ {
-		ll.edges[i] = newIntSet()
-	}
-	return ll
+func (s pairSet) add(item pair) {
+	s[item] = mark
 }
 
-func (ll *basicLinkedList) addEdge(a, b int) {
-	ll.addDirectedEdge(a, b)
-	ll.addDirectedEdge(b, a)
-}
-
-func (ll *basicLinkedList) addDirectedEdge(a, b int) {
-	if ll.edges[a].add(b) {
-		ll.deg++
-	}
-}
-
-func (ll *basicLinkedList) exists(a, b int) bool {
-	return ll.edges[a].doesContain(b)
-}
-
-func nextLinkedList(n, m, offset int, sc *bufio.Scanner) *basicLinkedList {
-	ll := newLinkedList(n)
-	for i := 0; i < m; i++ {
-		x, y := nextInt(sc), nextInt(sc)
-
-		x -= offset
-		y -= offset
-		ll.addEdge(x, y)
-	}
-	return ll
-}
-
-func nextDirectedLinkedList(n, m, offset int, sc *bufio.Scanner) *basicLinkedList {
-	ll := newLinkedList(n)
-	for i := 0; i < m; i++ {
-		x, y := nextInt(sc), nextInt(sc)
-
-		x -= offset
-		y -= offset
-		ll.addDirectedEdge(x, y)
-	}
-	return ll
-}
-
-type intSet map[int]none
-
-func newIntSet() intSet {
-	return map[int]none{}
-}
-
-func (s intSet) add(i int) (added bool) {
-	_, ok := s[i]
-	added = !ok
-	s[i] = mark
-	return
-}
-
-func (s intSet) remove(i int) (removed bool) {
-	_, removed = s[i]
-	delete(s, i)
-	return
-}
-
-func (s intSet) doesContain(i int) bool {
-	_, ok := s[i]
+func (s pairSet) doesContain(item pair) bool {
+	_, ok := s[item]
 	return ok
 }
 
-func (s intSet) size() int {
+func (s pairSet) size() int {
 	return len(s)
 }
 
-func (s intSet) members() chan int {
-	ch := make(chan int)
+func (s pairSet) members() (l []pair) {
+	for k := range s {
+		l = append(l, k)
+	}
+	return l
+}
+
+var mark none
+
+type none struct{}
+
+// package: gocom
+// packed src of [/Users/masumi/dev/src/github.com/masu-mi/gamemo/lib/gocom/exhaustive_permutations.go] with goone.
+
+func permutations(l int) chan []int {
+	ch := make(chan []int)
 	go func() {
-		defer close(ch)
-		for k := range s {
-			ch <- k
-		}
+		dfsPermutations(0, make([]bool, l), []int{}, func(perm []int) bool {
+			ch <- perm
+			return false
+		})
+		close(ch)
 	}()
 	return ch
+}
+
+func dfsPermutations(pos int, used []bool, perm []int, atLeaf func(perm []int) (halt bool)) (halt bool) {
+	l := len(used)
+	if pos == l {
+		p := append(perm[:0:0], perm...)
+		return atLeaf(p)
+	}
+
+	for i := 0; i < l; i++ {
+		if used[i] {
+			continue
+		}
+		used[i] = true
+		if dfsPermutations(pos+1, used, append(perm, i), atLeaf) {
+			return true
+		}
+		used[i] = false
+	}
+	return false
 }
 
 // snip-scan-funcs
