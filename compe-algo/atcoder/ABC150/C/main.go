85b11fc97bc16eb86e7139a033ca3ed075e2794c
Kanai Masumi <masumi.net@gmail.com>
2021-01-21 16:08:59 +0900

    'Add directory "ABC150/C" with addir.sh'

--- compe-algo/atcoder/ABC150/C/main.go
+++ compe-algo/atcoder/ABC150/C/main.go
@@ -8,50 +8,44 @@ import (
 	"strconv"
 )
 
-func main() {
-	fmt.Printf("%d\n", resolve(parseProblem(os.Stdin)))
-}
+const (
+	initialBufSize = 100000
+	maxBufSize     = 1000000
+)
 
-func parseProblem(r io.Reader) (n int, a, b []int) {
-	const (
-		initialBufSize = 100000
-		maxBufSize     = 1000000
-	)
+var sc *bufio.Scanner
+
+func initScanner(r io.Reader) *bufio.Scanner {
 	buf := make([]byte, initialBufSize)
 
 	sc := bufio.NewScanner(r)
 	sc.Buffer(buf, maxBufSize)
 	sc.Split(bufio.ScanWords) // bufio.ScanLines
-
-	n = scanInt(sc)
-	return n, scanIntSlice(sc, n), scanIntSlice(sc, n)
+	return sc
 }
 
-func scanIntSlice(sc *bufio.Scanner, n int) (s []int) {
-	for i := 0; i < n; i++ {
-		s = append(s, scanInt(sc)-1)
+func main() {
+	sc = initScanner(os.Stdin)
+	fmt.Println(resolve())
+}
+
+func resolve() int {
+	n := scanInt(sc)
+	p := nextIntSlice(sc, n)
+	q := nextIntSlice(sc, n)
+	sp, tp := 0, 0
+	idx := 0
+	for perm := range permutations(n) {
+		if match(perm, p) {
+			sp = idx
+		}
+		if match(perm, q) {
+			tp = idx
+		}
+		idx++
 	}
-	return s
+	return abs(sp - tp)
 }
-
-func resolve(n int, a, b []int) int {
-	numA, numB := -1, -1
-	i := 0
-	for l := range permutations(n) {
-		if match(l, a) {
-			numA = i
-		}
-		if match(l, b) {
-			numB = i
-		}
-		if numA > -1 && numB > -1 {
-			break
-		}
-		i++
-	}
-	return abs(numA - numB)
-}
-
 func abs(v int) int {
 	if v < 0 {
 		return -v
@@ -59,25 +53,17 @@ func abs(v int) int {
 	return v
 }
 
-func match(a, b []int) bool {
-	for i := 0; i < len(a); i++ {
-		if a[i] != b[i] {
+func match(perm0, target1 []int) bool {
+	for i := range perm0 {
+		if perm0[i] != target1[i]-1 {
 			return false
 		}
 	}
 	return true
 }
 
-// snip-scan-funcs
-func scanInt(sc *bufio.Scanner) int {
-	sc.Scan()
-	i, _ := strconv.Atoi(sc.Text())
-	return int(i)
-}
-func scanString(sc *bufio.Scanner) string {
-	sc.Scan()
-	return sc.Text()
-}
+// package: gocom
+// packed src of [/Users/masumi/dev/src/github.com/masu-mi/gamemo/lib/gocom/exhaustive_permutations.go] with goone.
 
 func permutations(l int) chan []int {
 	ch := make(chan []int)
@@ -96,9 +82,7 @@ func permutations(l int) chan []int {
 func dfsPermutations(pos int, used []bool, perm []int, atLeaf func(perm []int) (halt bool)) (halt bool) {
 	l := len(used)
 	if pos == l {
-		if atLeaf(perm) {
-			return true
-		}
+		return atLeaf(perm)
 	}
 
 	for i := 0; i < l; i++ {
@@ -112,4 +96,38 @@ func dfsPermutations(pos int, used []bool, perm []int, atLeaf func(perm []int) (
 		used[i] = false
 	}
 	return false
+}
+
+// package: gocom
+// packed src of [/Users/masumi/dev/src/github.com/masu-mi/gamemo/lib/gocom/next.go] with goone.
+
+func nextInt(sc *bufio.Scanner) int {
+	sc.Scan()
+	a, _ := strconv.Atoi(sc.Text())
+	return int(a)
+}
+
+func nextString(sc *bufio.Scanner) string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextIntSlice(sc *bufio.Scanner, n int) (a []int) {
+
+	a = make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = nextInt(sc)
+	}
+	return a
+}
+
+// snip-scan-funcs
+func scanInt(sc *bufio.Scanner) int {
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	return int(i)
+}
+func scanString(sc *bufio.Scanner) string {
+	sc.Scan()
+	return sc.Text()
 }
